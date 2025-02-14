c69f699c2612112a145194fe34dbfd7ca1eb5631
riiansmart <rismart1221@outlook.com>
2025-02-13 19:25:34 -0700

    updated _Layout to display unique navbar based on account role sign in (Admin sees admin features, User sees user features)

--- obj/Debug/net8.0/CardMaxxing.AssemblyInfo.cs
+++ obj/Debug/net8.0/CardMaxxing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardMaxxing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8faf4e58c978fcd469b54f639a1af8d5ab63d375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc70f707a87eb7217af10d67ab2e42ab752963b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardMaxxing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardMaxxing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
