9dbfa8454d11b20dd1efa3a119dce6b6f32623da
riiansmart <rismart1221@outlook.com>
2025-04-05 12:54:10 -0700

    Update homepage header and assembly version info

--- obj/Debug/net8.0/CardMaxxing.AssemblyInfo.cs
+++ obj/Debug/net8.0/CardMaxxing.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardMaxxing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884cf1bb13281e45441b370a7947d5f5d5ef358e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a8776328b709cb7c3eb195e197cc678a0a8945")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardMaxxing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardMaxxing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
